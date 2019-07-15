434cba76ded3fa3a5cf26b99a23d8e2de9409339
hossam20052@hotmail.com <Dubai2019>
2019-07-15 08:47:19 +0400

    Second Commit

--- app/src/main/java/com/example/sharingapp/AddContactActivity.java
+++ app/src/main/java/com/example/sharingapp/AddContactActivity.java
@@ -17,6 +17,9 @@ public class AddContactActivity extends AppCompatActivity {
     private EditText username;
     private EditText email;
 
+    public void test() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
